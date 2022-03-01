fd4b7a50892289418c80d2317d5cf016b2befe61
Evgeniy <jon83@inbox.ru>
2022-03-01 10:04:01 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.Write("Введите число для проверки кратности: ");
+int N = Convert.ToInt16(Console.ReadLine());
+int M = 7;
+int S = 23;
+int result_1 = N % M;
+int result_2 = N % S;
+if (result_1 == 0 & result_2 == 0)
+{
+    Console.WriteLine($"Число {N} кратно {M} и {S}");
+}
+else
+{
+    if (result_1 == 0 & result_2 != 0)
+    {
+        Console.WriteLine($"Число {N} кратно {M} и не кратно {S}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {N}  не кратно {M} и кратно {S}");
+    }
+}
